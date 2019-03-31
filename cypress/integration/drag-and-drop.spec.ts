f1eb89cc8a691c4832960f4256dd7bae9a9b974e
Alex Bjørlig <alexbjorlig@icloud.com>
2019-03-31 12:04:46 +0200

    added wait to make sure stackblitz work

--- cypress/integration/drag-and-drop.spec.ts
+++ cypress/integration/drag-and-drop.spec.ts
@@ -1,5 +1,6 @@
 it('🔥 Thx to Stackoverflow, drag and drop support now works 🔥', () => {
     cy.visit('')
+    .wait(4000)
     .url().should('contain', 'angular')
     .get('h2').should('contain', 'To do');
 
